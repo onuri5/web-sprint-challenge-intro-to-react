e099c436aaa54ca5d74827af1dfae51a2671aa86
onuri5 <jomibaofficial@gmail.com>
2021-08-13 03:07:28 -0500

    finished the inital names container

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,48 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components'
+
+const ContainerDiv = styled.div `
+    background-color: ${pr => pr.theme.white};
+    padding: 10px;
+    width: 30%;
+    border-radius: 90px;
+    display: flex;
+    justify-content: center;
+    align-content: flex-start;
+    margin: 0 auto;
+    color: ${pr => pr.theme.primaryColor};
+    background: url('https://mcdn.wallpapersafari.com/medium/81/95/Z7D2q6.jpg');
+`
+
+const CharacterDiv = styled.div `
+    margin: 20px;
+    text-shadow: 0px 1px yellow;
+`
+
+const DetailsButton = styled.button `
+   color: ${pr => pr.theme.primaryColor};
+   background-color: black;
+`
+
+const Character = props => {
+    return (
+        <ContainerDiv>
+
+        <div>{
+            props.characters !== null ? 
+                props.characters.map((item, index) => {
+                return <div>
+                            <CharacterDiv key={index}>{item.name}</CharacterDiv>
+                            <DetailsButton>Details</DetailsButton>
+                        </div>})  
+            :   
+                ''}
+        </div>
+
+
+        </ContainerDiv>
+    )
+}
+
+export default Character;
---
+++ src/theme/index.js
@@ -0,0 +1,10 @@
+/* eslint-disable import/no-anonymous-default-export */
+
+export default {
+    primaryColor: '#ffe81f', 
+    secondaryColor: 'crimson', 
+    tertiaryColor: 'lemonchiffon',
+    breakpointMobile: '(max-width: 550px)',
+    black: 'black',
+    white: 'white'
+}
